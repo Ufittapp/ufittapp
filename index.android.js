5ecc70391039c3d8d13e1bfe37b68c525973ea42
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-18 10:12:42 -0600

    intial project structure

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native'
+import UfittApp from './App'
+
+AppRegistry.registerComponent('ufittapp', () => UfittApp);
