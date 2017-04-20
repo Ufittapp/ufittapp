c14d85499192ec6eda66665a449fbfa8904673ff
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-20 11:31:25 -0600

    project structure to use react native 0.41.2 version instead of latest one

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import SignupScreen from './app/containers/SignupScreen'
+//import SignupScreen from './app/containers/SignupScreen'
+import { Text } from 'react-native'
 
 export default class App extends React.Component{
     render(){
+        //<SignupScreen />
         return (
-            <SignupScreen />
+            <Text>hello world</Text>
         );
     }
 }
