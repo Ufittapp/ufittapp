19f2c87aa26a5bd72cbddc7da791d0abab142344
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-20 12:10:50 -0600

    create the redux store

---
+++ app/store/configureStore.js
@@ -0,0 +1,16 @@
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from '../reducers'
+
+
+// eslint-disable-next-line
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__});
+
+export default function configureStore(initialState){
+	const enhancer = compose(
+        applyMiddleware(thunkMiddleware) //loggerMiddleware
+    )
+
+	return createStore(reducer, initialState, enhancer)
+}
