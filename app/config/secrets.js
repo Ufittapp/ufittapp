bf8e9b0c9f96d3550759f4f9b761ea1f7832d7f8
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-26 10:32:41 -0600

    include firebase secrets

---
+++ app/config/secrets.js
@@ -0,0 +1,8 @@
+export const firebaseConfigs = {
+  apiKey: "AIzaSyAozqTok9RVl6NOtEqKZz5Gt7GoJw3hjcc",
+  authDomain: "ufitt-b51d8.firebaseapp.com",
+  databaseURL: "https://ufitt-b51d8.firebaseio.com",
+  projectId: "ufitt-b51d8",
+  storageBucket: "ufitt-b51d8.appspot.com",
+  messagingSenderId: "505581301035"
+}
