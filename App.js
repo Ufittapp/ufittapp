1614cee09f474f94966f56359135918b2904ee24
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-27 16:46:13 -0600

    initialize firebase app from entry point file

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import SignupScreen from './app/containers/SignupScreen'
 import { Provider } from 'react-redux'
-import { fromJS } from 'immutable'
+//import { fromJS } from 'immutable'
 import configureStore from './app/store/configureStore'
-const store = configureStore(fromJS({}))
+import firebase from 'firebase'
+import { firebaseConfigs } from '../config/secrets'
+
+firebase.initializeApp(firebaseConfigs)
+
+const store = configureStore()
 
 export default class App extends React.Component{
+    
     render(){
         return (
             <Provider store={store}>
