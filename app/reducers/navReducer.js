4b5ce08e155247a119273121a66f2fae1b48e8c2
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-27 19:47:44 -0600

    create the navigator header to use redux

---
+++ app/reducers/navReducer.js
@@ -0,0 +1,11 @@
+import { NavigationActions } from 'react-navigation'
+import { AppNavigator } from '../navigator/appNavigator'
+
+const initialNavState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Signup'))
+
+export default function navigatorReducer(state = initialNavState, action){
+    const nextState = AppNavigator.router.getStateForAction(action, state);
+
+    // Simply return the original `state` if `nextState` is null or undefined.
+    return nextState || state;
+}
