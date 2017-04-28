245c1a611d27ac34534a3ac4173a9cd414bfbdf0
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-04-27 18:55:57 -0600

    rename app file

--- ApplicationRoot.js
+++ ApplicationRoot.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import SignupScreen from './app/containers/SignupScreen'
+import * as screens from './app/navigation/ApplicationScreens'
 import { Provider } from 'react-redux'
 //import { fromJS } from 'immutable'
 import configureStore from './app/store/configureStore'
 import firebase from 'firebase'
 import { firebaseConfigs } from './app/config/secrets'
+import { StackNavigator } from 'react-navigation';
 
 firebase.initializeApp(firebaseConfigs)
 
 const store = configureStore()
 
-export default class App extends React.Component{
+export default class Root extends React.Component{
     
     render(){
         return (
