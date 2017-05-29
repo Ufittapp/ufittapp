eb47e0d9b8625b749fb39c036d17c23846f29795
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-05-29 12:46:52 -0600

    home screen

---
+++ app/assets/styles/home.js
@@ -0,0 +1,97 @@
+
+import { Dimensions } from 'react-native'
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
+export default {
+
+
+         headerBg: {
+           backgroundColor: '#212121',
+         },
+
+         whiteText: {
+           color: '#ffffff'
+         },
+
+         footerBg: {
+           backgroundColor: '#7a1405'
+         },
+
+         feedContainer: {
+           marginTop: 15,
+           marginBottom: 15
+         },
+
+         feedHeader: {
+           margin: 15,
+           flexDirection: 'row'
+         },
+
+         userFeed: {
+           flex: 1,
+           flexDirection: 'row',
+           alignItems: 'flex-start',
+         },
+
+         timeFeed: {
+           width: 50,
+           flexDirection: 'row',
+           justifyContent: 'flex-end',
+           alignItems: 'flex-start'
+         },
+
+         userName: {
+           marginLeft: 10
+         },
+
+         boldName: {
+           color: '#7d7885',
+           fontWeight: 'bold',
+           fontSize: 15
+         },
+
+         status: {
+           color: '#beb9c1',
+           fontSize: 13,
+           fontWeight: 'bold'
+         },
+         clockText: {
+           color: '#beb9c1',
+           fontSize: 18,
+           fontWeight: 'bold',
+           marginRight: 5
+         },
+         feedImg: {
+           height: 220,
+           width: deviceWidth
+         },
+
+         footerFeedIcons: {
+           color: '#7d7885',
+           fontWeight: 'bold',
+           fontSize: 24
+         },
+
+         footerFeedText: {
+           color: '#7d7885',
+           fontWeight: 'bold',
+           fontSize: 14,
+           marginLeft: 5,
+         },
+
+         footerFeed: {
+           margin: 15,
+           flexDirection: 'row',
+           marginTop: 0
+         },
+
+         iconContainer: {
+           flexDirection: 'row',
+           width: deviceWidth / 4,
+           alignItems: 'center',
+         }
+
+
+     }
--- app/containers/HomeScreen.js
+++ app/containers/HomeScreen.js
@@ -1,18 +1,88 @@
 import React from 'react'
-import { Text, Container, Content, Footer } from 'native-base'
 import FooterTabs from '../common/FooterTabs'
 import { connect } from 'react-redux'
+import styles from '@assets/styles/home'
+  import { Container, Content, ListItem, Text, CheckBox, Header, Left, Button, Icon, Body, Title, Right,
+    InputGroup, Input, Form, Label, Item, Picker, Footer, FooterTab, Thumbnail} from 'native-base';
+import { View, Image } from 'react-native'
+
 
 class HomeScreen extends React.Component{
+    static navigationOptions = {
+        tabBarLabel: 'Feeds'
+    }
+
     render(){
         const { navigate } = this.props.navigation;
 
         return(
             <Container>
-                <Content>
-                    <Text>Feeds section</Text>
-                </Content>
-            </Container>
+                 <Header style={styles.headerBg}>
+                     <Left>
+                         <Button transparent>
+                             <Icon name='menu' style={styles.whiteText} />
+                         </Button>
+                     </Left>
+                     <Body>
+                         <Title style={styles.whiteText}>Feed</Title>
+                     </Body>
+                     <Right>
+                         <Button transparent>
+                             <Icon name='more' style={styles.whiteText} />
+                         </Button>
+                     </Right>
+                 </Header>
+
+                 <Content>
+                    <View style={styles.feedContainer}>
+                        <View style={styles.feedHeader}>
+
+                            <View style={styles.userFeed}>
+
+                              <View><Thumbnail size={40} source={require('@assets/images/feed_img.png')} /></View>
+                                <View style={styles.userName}>
+                                    <Text style={styles.boldName}>James Wilson</Text>
+                                    <Text style={styles.status}>posted</Text>
+                                </View>
+
+                            </View>
+
+                            <View style={styles.timeFeed}>
+                              <Icon name='clock' style={styles.clockText} />
+                              <Text style={styles.status}>2h</Text>
+                            </View>
+
+                        </View>
+
+                        <View style={styles.feedSlide}>
+                          <Image source={require('@assets/images/feed_img.png')} style={styles.feedImg}/>
+                        </View>
+                    </View>
+
+                    <View style={styles.footerFeed}>
+                        <View style={styles.iconContainer}>
+                          <Icon name='heart' style={styles.footerFeedIcons} />
+                          <Text style={styles.footerFeedText}>2K</Text>
+                        </View>
+
+                        <View style={styles.iconContainer}>
+                          <Icon name='chatboxes' style={styles.footerFeedIcons} />
+                          <Text style={styles.footerFeedText}>98</Text>
+                        </View>
+
+                        <View style={styles.iconContainer}>
+                          <Icon name='md-share' style={styles.footerFeedIcons} />
+                          <Text style={styles.footerFeedText}>69</Text>
+                        </View>
+
+                        <View style={styles.iconContainer}>
+                          <Icon name='person' style={styles.footerFeedIcons} />
+                          <Text style={styles.footerFeedText}>profile</Text>
+                        </View>
+                    </View>
+                 </Content>
+
+                 </Container>
         )
     }
 }
