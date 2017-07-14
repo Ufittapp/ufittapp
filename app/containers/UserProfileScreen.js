241e941c25e3c8c9007f473c64024e6d16e2a814
Nahum Fabian <nahum_fabian13@hotmail.com>
2017-07-14 12:33:11 -0600

    save user profile image to firebase storage

--- app/containers/UserProfileScreen.js
+++ app/containers/UserProfileScreen.js
@@ -1,10 +1,73 @@
 import React from 'react'
-import { Text, Icon } from 'native-base'
+import { Text, Icon, Container, Content } from 'native-base'
+import { TouchableWithoutFeedback, Image } from 'react-native'
+var ImagePicker = require('react-native-image-picker');
+import FirebaseImageManager from '../utils/FirebaseImageManager'
+
+var options = {
+  title: 'Select Avatar',
+  customButtons: [
+    {name: 'fb', title: 'Choose Photo from Facebook'},
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
 
 class UserProfileScreen extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.pickImageFromDevice = this.pickImageFromDevice.bind(this)
+
+        this.state = {
+            imageUri: 'http://via.placeholder.com/350x150'
+        }
+    }
+
+    pickImageFromDevice(){
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            }
+            else {
+                // let source = { uri: response.uri };
+                // this.setState({image_uri: response.uri})
+
+                // You can also display the image using data:
+                // let image_uri = { uri: 'data:image/jpeg;base64,' + response.data };
+
+            FirebaseImageManager
+                .uploadImageToStorage(response.uri)
+                .then(url => { 
+                    console.log('uploaded', url); 
+                    this.setState({imageUri: url}) })
+                .catch(error => console.log(error))
+            }
+        })
+    }
+
+
     render(){
         return (
-            <Text>Profile screen</Text>
+            <Container style={{alignItems: 'center'}}>
+                <Content >
+                    <TouchableWithoutFeedback onPress={this.pickImageFromDevice}>
+                        <Image
+                            style={{width: 100, height: 100, borderRadius: 50}}
+                            source={{uri: this.state.imageUri}} />
+                    </TouchableWithoutFeedback>
+                </Content>
+            </Container>
         )
     }
 }
---
+++ app/utils/FirebaseImageManager.js
@@ -0,0 +1,42 @@
+import firebase from 'firebase'
+import RNFetchBlob from 'react-native-fetch-blob'
+import { Platform } from 'react-native'
+
+// Prepare Blob support
+const Blob = RNFetchBlob.polyfill.Blob
+const fs = RNFetchBlob.fs
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+window.Blob = Blob
+
+class FirebaseImageManager{
+    static uploadImageToStorage(uri, mime = 'application/octet-stream'){
+        return new Promise((resolve, reject) => {
+            
+            const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
+            let uploadBlob = null
+            const imageName = firebase.auth().currentUser.uid
+
+            const imageRef = firebase.storage()
+                .ref()
+                .child('profileImages')
+                .child(imageName)
+
+            fs.readFile(uploadUri, 'base64')
+                .then((data) => {
+                    return Blob.build(data, { type: `${mime};BASE64` })
+                })
+                .then(blob => {
+                    uploadBlob = blob
+                    return imageRef.put(blob, { contentType: 'image/jpeg' })
+                })
+                .then(() => {
+                    uploadBlob.close()
+                    return imageRef.getDownloadURL()
+                })
+                .then(url => resolve(url))
+                .catch(error=> reject(error))
+            })
+        }
+    }
+
+export default FirebaseImageManager
